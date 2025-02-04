24545eb9b609e7a0b329702fdf89d333d412b2ec
Cla Mattia Galliard <clamattia@gmail.com>
2025-02-04 07:55:10 +0100

    UDP echo client/server

--- client/client.go
+++ client/client.go
@@ -2,12 +2,45 @@ package main
 
 import (
     "log"
-    "mesher/msg"
+    // "mesher/msg"
+    "net"
+    "strconv"
+    "time"
 )
 
 func main() {
 
-    log.Println(msg.Client_hello)
+    server_addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8981")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    local_addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    conn, err := net.DialUDP("udp", local_addr, server_addr)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    i := 0
+    for {
+        msg := strconv.Itoa(i)
+        i++
+        txbuf := []byte(msg)
+        _, err := conn.Write(txbuf)
+        if err != nil {
+            log.Println(msg, err)
+        }
+        time.Sleep(time.Second * 1)
+        rxbuf := make([]byte, 65536)
+        n, addr, err := conn.ReadFromUDP(rxbuf)
+        log.Println("Received ", string(rxbuf[0:n]), " from ", addr)
+    }
+
+    conn.Close()
 
 }
 
--- server/server.go
+++ server/server.go
@@ -2,13 +2,31 @@ package main
 
 import (
     "log"
-    "mesher/msg"
+    // "mesher/msg"
+    "net"
 )
 
 func main() {
+    port := ":8981"
+    local_addr, err := net.ResolveUDPAddr("udp", port)
+    if err != nil {
+        log.Fatal(err)
+    }
+    conn, err := net.ListenUDP("udp", local_addr)
+    if err != nil {
+        log.Fatal(err)
+    }
+    buf := make([]byte, 65536)
 
-    log.Println(msg.Server_hello)
-
+    for {
+        n, addr, err := conn.ReadFromUDP(buf)
+        if err != nil {
+            log.Println("Error: ", err)
+            continue
+        }
+        log.Println("Received ", string(buf[0:n]), " from ", addr)
+        // rx <- buf[0:n]
+        conn.WriteToUDP(buf[0:n], addr)
+    }
+    conn.Close()
 }
-
-
